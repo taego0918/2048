f0998e8962156fd2182a0a660d4fe7d4ba292f75
TaeGo <taego0918@gmail.com>
2019-08-01 17:43:53 +0800

    no message

--- public/js/2048.js
+++ public/js/2048.js
@@ -1,6 +1,7 @@
-let Game = function(obj){
+let Game = function(obj,_socket){
     const self = this;
     self.obj = obj;
+    self.socket = _socket;
     self.gridWidth  = obj.offsetWidth / 5;
     self.gridHeight = obj.offsetHeight / 5;
     self.lineHeight = self.gridHeight / 5 * 4;
@@ -13,10 +14,20 @@ let Game = function(obj){
     self.previousPos;
     self.point = 0;
     self.topPoint = 0;
-    
+    self.isMobile = false;
+    self.isMobile = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
     let gameOver = document.getElementsByClassName('gameOver')[0];
     let point = document.getElementById('point');
     let topPoint = document.getElementById('top-point');
+
+    let mousedown   = 'mousedown' ,
+        mouseup     = 'mouseup',
+        mousemove   = 'mousemove';
+    if(self.isMobile){
+        mousedown   = 'touchstart' ,
+        mouseup     = 'touchend',
+        mousemove   = 'touchmove';
+    }
     
     self.getRandown = (minNum,maxNum)=>{
         return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
@@ -29,34 +40,22 @@ let Game = function(obj){
 
     self.mouseMoveChk = (e)=>{
         if(typeof self.previousPos === 'undefined')return;
-        let previousPos = self.getMousePos(e);
-        let direction;
+        let previousPos , direction;
 
-        if(Math.abs(self.previousPos.x  - previousPos.x) >= self.moveChkDistance){
-            (self.previousPos.x  > previousPos.x)? direction ='left': direction ='right';
-        }else if(Math.abs(self.previousPos.y - previousPos.y) >= self.moveChkDistance){
-            (self.previousPos.y  > previousPos.y)?direction ='up':direction ='down';
-        }
-
-        if(typeof direction !== 'undefined'){
-            self.gridMove(direction);
-            delete self.previousPos;
-        }
-    }
-
-    self.touchMoveChk = (e)=>{
-        if(typeof self.previousPos === 'undefined')return;
-        let previousPos = { x : e.touches[e.touches.length-1].clientX ,
+        if(!self.isMobile){
+            previousPos = self.getMousePos(e);
+        }else{
+            previousPos = { x : e.touches[e.touches.length-1].clientX ,
                             y : e.touches[e.touches.length-1].clientY }
-        let direction;
+        }
 
         if(Math.abs(self.previousPos.x  - previousPos.x) >= self.moveChkDistance){
             (self.previousPos.x  > previousPos.x)? direction ='left': direction ='right';
         }else if(Math.abs(self.previousPos.y - previousPos.y) >= self.moveChkDistance){
             (self.previousPos.y  > previousPos.y)?direction ='up':direction ='down';
         }
-
         if(typeof direction !== 'undefined'){
+            self.gameType = 'moveing';
             self.gridMove(direction);
             delete self.previousPos;
         }
@@ -75,25 +74,37 @@ let Game = function(obj){
         }
     }
     self.classReplace = (_original,_later,_power)=>{
-        let div = document.getElementsByClassName(_original)[0];
-        let later = document.getElementsByClassName(_later)[0];
-
-        div.className = '';
-        div.classList.add(_original);
-        if(_power<11){
-            div.classList.add('power'+_power);
+        let div     = document.getElementsByClassName('pos-'+ _original)[0];
+        let later   = document.getElementsByClassName('pos-'+ _later)[0];
+        let timeout;
+        if( (_original%5) === (_later%5)){
+            timeout = parseFloat(Math.abs(_original/5 - _later/5) * 0.001 * self.moveSpacing);
+            later.style.transition = timeout +'s';
+            later.style.top = parseInt( parseInt(_original / 5) * self.gridHeight )+ 'px';
         }else{
-            div.classList.add('power11');
+            timeout = parseFloat(Math.abs(_original%5 - _later%5) * 0.001 * self.moveSpacing);
+            later.style.transition = timeout +'s';
+            later.style.left = parseInt( parseInt(_original % 5) * self.gridWidth )+ 'px';
         }
-        div.innerHTML= Math.pow(2,_power);
-        self.point += Math.pow(2,_power);
 
-        point.innerHTML = self.point;
-        if(self.point> self.topPoint){
-            self.topPoint = self.point;
-            topPoint.innerHTML = self.topPoint;
-        }
-        self.obj.removeChild(later);
+        setTimeout(() => {
+            self.obj.removeChild(later);
+            div.className = '';
+            div.classList.add('pos-'+ _original);
+            if(_power<11){
+                div.classList.add('power'+_power);
+            }else{
+                div.classList.add('power11');
+            }
+            div.innerHTML= Math.pow(2,_power);
+            self.point += Math.pow(2,_power);
+
+            point.innerHTML = self.point;
+            if(self.point> self.topPoint){
+                self.topPoint = self.point;
+                topPoint.innerHTML = self.topPoint;
+            } 
+        }, (timeout -0.1) *1000);
     }
     self.gridMove = (_direction) =>{
         switch(_direction){
@@ -114,8 +125,8 @@ let Game = function(obj){
                                     item = 5;
                                 }else if( self.gridList[item * 5 + row] === self.gridList[col * 5 + row]){
                                     self.gridList[item * 5 + row]++;
-                                    self.classReplace('pos-'+ parseInt(item * 5 + row),
-                                                        'pos-'+ parseInt(col * 5 + row),
+                                    self.classReplace(  item * 5 + row,
+                                                        col * 5 + row,
                                                         self.gridList[item * 5 + row]);
                                     delete self.gridList[col * 5 + row];
                                     item = 5;
@@ -142,8 +153,8 @@ let Game = function(obj){
                                     item = -1;
                                 }else if( self.gridList[item * 5 + row] === self.gridList[col * 5 + row]){
                                     self.gridList[item * 5 + row]++;
-                                    self.classReplace('pos-'+ parseInt(item * 5 + row),
-                                                        'pos-'+ parseInt(col * 5 + row),
+                                    self.classReplace(  item * 5 + row,
+                                                        col * 5 + row,
                                                         self.gridList[item * 5 + row]);
                                     delete self.gridList[col * 5 + row];
                                     item = -1;
@@ -170,8 +181,8 @@ let Game = function(obj){
                                     item = 6;
                                 }else if( self.gridList[col * 5 + item] === self.gridList[col * 5 + row]){
                                     self.gridList[col * 5 + item]++;
-                                    self.classReplace(  'pos-'+ parseInt(col * 5 + item),
-                                                        'pos-'+ parseInt(col * 5 + row),
+                                    self.classReplace(  col * 5 + item,
+                                                        col * 5 + row,
                                                         self.gridList[col * 5 + item]);
                                     delete self.gridList[col * 5 + row];
                                     item = 6;
@@ -198,8 +209,8 @@ let Game = function(obj){
                                     item=-1;
                                 }else if( self.gridList[col * 5 + item] === self.gridList[col * 5 + row]){
                                     self.gridList[col * 5 + item]++;
-                                    self.classReplace(  'pos-'+ parseInt(col * 5 + item),
-                                                        'pos-'+ parseInt(col * 5 + row),
+                                    self.classReplace(  col * 5 + item,
+                                                        col * 5 + row,
                                                         self.gridList[col * 5 + item]);
                                     delete self.gridList[col * 5 + row];
                                     item=-1;
@@ -215,33 +226,25 @@ let Game = function(obj){
         setTimeout(() => {
             self.gameType = 'wait';
             self.newGrid();
-        }, self.moveSpacing * 4);
+        }, self.moveSpacing * 3);
     }
     
-    self.obj.addEventListener('mousedown',(e)=>{
+    self.obj.addEventListener(mousedown,(e)=>{
         if(self.gameType !=='wait'){return;}
-        self.previousPos = self.getMousePos(e);
-        self.obj.addEventListener('mousemove', (e)=>{
+        if(!self.isMobile){
+            self.previousPos = self.getMousePos(e);
+        }else{
+            self.previousPos =  {  x : e.touches[0].clientX,
+                                   y : e.touches[0].clientY};
+        }
+        self.obj.addEventListener(mousemove, (e)=>{
+            if(self.gameType !=='wait'){return;}
             if(typeof self.previousPos === 'undefined')return;
-            self.gameType = 'moveing';
             self.mouseMoveChk(e);
         })
     })
-    self.obj.addEventListener('touchstart',(e)=>{
-        if(self.gameType !=='wait'){return;}
-        self.previousPos =  { x : e.touches[0].clientX,
-                              y : e.touches[0].clientY};
-        self.obj.addEventListener('touchmove', (e)=>{
-            if(typeof self.previousPos === 'undefined')return;
-            self.gameType = 'moveing';
-            self.touchMoveChk(e);
-        })
-    })
 
-    self.obj.addEventListener('mouseup',()=>{
-        delete self.previousPos;
-    })
-    self.obj.addEventListener('touchend',()=>{
+    self.obj.addEventListener(mouseup,()=>{
         delete self.previousPos;
     })
     
@@ -263,7 +266,8 @@ let Game = function(obj){
     }
     
     self.chkOverGrid = () =>{
-        let returnData = false;
+        let returnData = false,
+            gridList = '';
         for(let item = 0;item < self.gridList.length;item++){
             if(typeof self.gridList[item] === "undefined"){
                 returnData = true;
@@ -274,6 +278,22 @@ let Game = function(obj){
             self.gameType = 'gameOver';
             gameOver.style.display = 'block';
         }
+        for(let item = 0;item < self.gridList.length;item++){
+            if(typeof self.gridList[item] === "undefined"){
+                gridList += '0'
+            }else{
+                gridList +=  self.gridList[item].toString(16);
+            }
+        }
+        let data = {
+            name : localStorage.getItem("name"),
+            point : self.point,
+            TPoint : self.topPoint,
+            gridList: gridList
+        }
+
+        self.socket.emit('gamerData',data);
+
         return returnData;
     }
 
@@ -293,13 +313,19 @@ let Game = function(obj){
 
     self.createGrid = (_position,_power)=>{
         let div = document.createElement('div');
+        let self = this;
         div.style.lineHeight = self.lineHeight + 'px';
         div.style.top = parseInt(_position / 5) * self.gridHeight + 'px';
         div.style.left = (_position % 5) * self.gridWidth  + 'px';
+        div.style.transition = '0.5s';
+        div.style.opacity = '0';
         div.innerHTML = Math.pow(2,_power);
         div.classList.add('power'+_power);
         div.classList.add('pos-'+_position);
         self.obj.appendChild(div);
+        setTimeout(() => {
+            div.style.opacity = '1';
+        }, 0);
     }
     self.reset();
 }
--- socket.js
+++ socket.js
@@ -4,11 +4,24 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 let connectionNum = 0;
+let OPDate = [];
 io.on('connection', (socket) => {
     connectionNum++;
     socket.on('connectNum',()=>{
         io.emit('connectNum',connectionNum);
     });
+    socket.on('gamerData',(obj)=>{
+        let isContain = false;
+        for(let item = 0 ;item < OPDate.length;item++){
+            if(OPDate[item].name === obj.name){
+                OPDate[item] = obj;
+                isContain = true;
+                break;
+            }
+        }
+        if(!isContain){OPDate.push(obj);}
+        io.emit('OPData',OPDate);
+    })
     socket.on("message", (obj) => {
         let returnData;
         let getTime = new Date();
